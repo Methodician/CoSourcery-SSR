ec01010075867dd65e2c6e84b78e1fca799beb66
Methodician <methodician@gmail.com>
2019-05-11 15:15:38 -0700

    Added some basic scaffolding and setup

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MetaComponent } from './meta/meta.component';
+import { FireComponent } from './fire/fire.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MetaComponent,
+    FireComponent
   ],
   imports: [
     BrowserModule,
