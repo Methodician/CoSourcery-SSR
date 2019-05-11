53be581347ed0d6101617f1dc2d6937ca95926b8
Methodician <methodician@gmail.com>
2019-05-11 15:23:25 -0700

    Added basic firebase test

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,17 +6,20 @@ import { AppComponent } from './app.component';
 import { MetaComponent } from './meta/meta.component';
 import { FireComponent } from './fire/fire.component';
 
+import { AngularFireModule } from '@angular/fire';
+import { AngularFirestoreModule } from '@angular/fire/firestore';
+
+import { environment } from '../environments/environment';
+
 @NgModule({
-  declarations: [
-    AppComponent,
-    MetaComponent,
-    FireComponent
-  ],
+  declarations: [AppComponent, MetaComponent, FireComponent],
   imports: [
+    AngularFireModule.initializeApp(environment.firebase),
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    AngularFirestoreModule,
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
--- src/app/fire/fire.component.ts
+++ src/app/fire/fire.component.ts
@@ -1,15 +1,18 @@
 import { Component, OnInit } from '@angular/core';
+import { AngularFirestore } from '@angular/fire/firestore';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'cos-fire',
   templateUrl: './fire.component.html',
-  styleUrls: ['./fire.component.scss']
+  styleUrls: ['./fire.component.scss'],
 })
 export class FireComponent implements OnInit {
+  items: Observable<any[]>;
 
-  constructor() { }
+  constructor(private fsdb: AngularFirestore) {}
 
   ngOnInit() {
+    this.items = this.fsdb.collection('items').valueChanges();
   }
-
 }
