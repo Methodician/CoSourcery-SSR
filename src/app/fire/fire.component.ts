1b6b50a1450a182bb9daf687f70fca5515bcd5ce
Methodician <methodician@gmail.com>
2019-05-11 15:59:04 -0700

    Cot carried away with meta test

--- src/app/fire/fire.component.ts
+++ src/app/fire/fire.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { Meta, Title } from '@angular/platform-browser';
 import { AngularFirestore } from '@angular/fire/firestore';
 import { Observable } from 'rxjs';
+import { take } from 'rxjs/operators';
 
 @Component({
   selector: 'cos-fire',
@@ -10,9 +12,32 @@ import { Observable } from 'rxjs';
 export class FireComponent implements OnInit {
   items: Observable<any[]>;
 
-  constructor(private fsdb: AngularFirestore) {}
+  constructor(
+    private afs: AngularFirestore,
+    private meta: Meta,
+    private title: Title,
+  ) {}
 
   ngOnInit() {
-    this.items = this.fsdb.collection('items').valueChanges();
+    this.updateMetaData();
+    this.items = this.afs.collection('items').valueChanges();
   }
+
+  updateMetaData = async () => {
+    const snaps = await this.afs
+      .collection('items')
+      .valueChanges()
+      .pipe(take(1))
+      .toPromise();
+
+    this.title.setTitle('snaps[0].name');
+    const stringifiedSnaps = snaps.map(
+      (item: any) => `a thing called ${item.name} with ${item.other} `,
+    );
+
+    this.meta.updateTag({
+      name: 'description',
+      content: stringifiedSnaps.toString(),
+    });
+  };
 }
